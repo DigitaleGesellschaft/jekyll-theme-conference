de77779745168a70100477f015da7c6a87e65a16
Lorenz Schmid <lorenzschmid@users.noreply.github.com>
2021-02-15 20:29:41 +0100

    Fix caching in build workflow example

--- _tools/purgecss.config.js
+++ _tools/purgecss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ['_site/**/*.html','_site/assets/js/main.js'],
   css: ['_site/assets/css/main.css'],
-  output: '_site/assets/css/main.min.css',
+  output: '_site/assets/css/main.css',
   fontFace: true,
   safelist: {
     deep: [/^leaflet/]
