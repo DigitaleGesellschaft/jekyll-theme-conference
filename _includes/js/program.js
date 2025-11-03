ed60320868355bca8cfd7cbbbc83a53289201048
Lorenz Schmid <lorenzschmid@users.noreply.github.com>
2025-11-03 11:54:01 +0100

    Fix automatic day switching in program

--- _includes/js/program.js
+++ _includes/js/program.js
@@ -6,33 +6,28 @@ window.conference.program = (() => {
     };
 
     const init = () => {
-        if ($("#day-list")) {
-            // Switch to day if page load with hash
-            const hash = window.location.hash;
-            if (hash) {
-                $('#day-list a[href="' + hash + '"]').tab("show");
-            }
+        const $dayList = $('#day-list');
+        if (!$dayList.length) return;
 
-            // Switch to day if today
-            else {
-                const now = new Date();
-                const tsNow = Math.floor(now.getTime() / 1000);
-
-                $('a[data-toggle="tab"]').each(function () {
-                    const tsMidnight = new Date($(this).data("ts") * 1000);
-
-                    if (tsMidnight <= tsNow < tsMidnight + 24 * 60 * 60) {
-                        $(this).tab("show");
-                        updateHash(this.hash);
-                    }
-                });
-            }
-
-            // Add current selected day as hash to URL while keeping current scrolling position
-            $('a[data-toggle="tab"]').on("shown.bs.tab", function () {
-                updateHash(this.hash);
+        const hash = window.location.hash;
+        if (hash) {
+            $(`a[data-toggle="tab"][href="${hash}"]`).tab('show');
+        } else {
+            const tsNow = Date.now() / 1000 | 0;
+            $('a[data-toggle="tab"]').each(function () {
+                const tsMidnight = $(this).data("ts");
+                if (tsMidnight && tsMidnight <= tsNow && tsNow < tsMidnight + 24 * 60 * 60) {
+                    $(this).tab('show');
+                    updateHash(this.hash);
+                    return false;
+                }
             });
         }
+
+        // Add current selected day as hash to URL while keeping current scrolling position
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function () {
+            updateHash(this.hash);
+        });
     };
 
     return {
